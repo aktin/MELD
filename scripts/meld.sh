0637ea0889b2b41ae1ac03a2abb7495c63e6777b
Simon Hüning <huening.simon@gmail.com>
2026-06-18 16:05:07 +0200

    change file extension yml to yaml for consistency

--- scripts/meld.sh
+++ scripts/meld.sh
@@ -6,7 +6,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 scripts=("run.sh"
           "utils.sh"
           "install.sh"
-          "compose.yml")
+          "compose.yaml")
 
 print_logo() {
   cat <<EOF
