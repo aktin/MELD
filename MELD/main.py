84aaeddd1e20671ce8d50da0cd926b03973ecf3b
Simon Hüning <huening.simon@gmail.com>
2026-06-22 14:26:45 +0200

    pass commands to container via docker compose run instead of env var

--- MELD/main.py
+++ MELD/main.py
@@ -1,24 +1,27 @@
 import os
+import sys
 
+from Logger import setup_logger
 from ModelManager import run_inference
 from ModelManager.manager import pull_runtime, remove_runtime
 
-cmd = os.environ.get("MELD_CMD")
 
-if not cmd:
-    raise Exception("MELD_CMD is not set")
+if __name__ == "__main__":
+    logger = setup_logger("meld")
 
-# if cmd == "train":
-#     pass
-#     # Minimal CLI: customize paths via env vars to avoid adding argparse right now.
-#     contract_path = os.getenv("CONTRACT_PATH", "/home/shuening/code/KlimaNot/examples/tfdf/resources/contract-training.yaml")
-#     query_path = os.getenv("QUERY_PATH", "/home/shuening/code/KlimaNot/examples/tfdf/resources/long-query-training.sql")
-#     artifact_path = os.getenv("ARTIFACT_PATH", "/home/shuening/code/KlimaNot/examples/tfdf/artifact_test")
-#     run_training(contract_path, query_path, artifact_path)
-# elif cmd == "run":
-if cmd == "run":
-    run_inference(contract_path="/resources/contract.yaml")
-elif cmd == "pull":
-    pull_runtime(contract_path="/resources/contract.yaml")
-elif cmd == "delete":
-    remove_runtime(contract_path="/resources/contract.yaml")
+
+    argv = sys.argv[1:]
+
+    if not argv:
+        logger.error("No command set")
+
+    cmd = argv[0]
+
+    if cmd == "run":
+        run_inference(contract_path="/resources/contract.yaml")
+    elif cmd == "pull":
+        pull_runtime(contract_path="/resources/contract.yaml")
+    elif cmd == "delete":
+        remove_runtime(contract_path="/resources/contract.yaml")
+    else:
+        logger.error(f"Unknown command: {cmd}")
--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 image_name="meld-orchestrator"
-version="0.1.1-alpha"
+version="0.2.0-alpha"
 
 cd ../MELD
 docker build -t "ghcr.io/simhue/$image_name:latest" \
