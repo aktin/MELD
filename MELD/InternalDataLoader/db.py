574777fb9bed2fe713466136ac70e45877c64ce4
Simon Hüning <huening.simon@gmail.com>
2026-07-16 15:39:13 +0200

    throw error if password file not found

--- MELD/InternalDataLoader/db.py
+++ MELD/InternalDataLoader/db.py
@@ -1,6 +1,9 @@
+import logging
 import os
 
 from sqlalchemy import create_engine
+
+logger = logging.getLogger("meld")
 
 host = os.getenv("DB_HOST")
 port = os.getenv("DB_PORT")
@@ -8,7 +11,11 @@ user = os.getenv("DB_USER")
 schema = os.getenv("DB_SCHEMA")
 password_file = os.getenv("DB_PASSWORD_FILE")
 
-with open(password_file, "r") as f:
-    password = f.readline().strip()
+try:
+    with open(password_file, "r") as f:
+        password = f.readline().strip()
+except FileNotFoundError:
+    logger.error(f"Database password file could not be found")
+    exit(1)
 
 engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{schema}")
