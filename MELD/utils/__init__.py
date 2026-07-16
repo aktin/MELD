18e7f8565fec3b9de106362188f74715e2dd63fb
Simon Hüning <huening.simon@gmail.com>
2026-07-16 15:40:23 +0200

    add data validation functions

--- MELD/utils/__init__.py
+++ MELD/utils/__init__.py
@@ -1,4 +1,5 @@
-from .utils import load_yaml
 from .utils import construct_image_ref
-from .utils import safe_filename_from_url
 from .utils import download_file
+from .utils import load_yaml
+from .utils import safe_filename_from_url
+from .validation import validate_required_features, validate_feature_datatypes, get_unexpected_features
