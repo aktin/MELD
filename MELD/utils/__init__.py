f440bbb013c272c559ac1f6087dd02d9119e7fc8
Simon Hüning <huening.simon@gmail.com>
2026-08-17 16:22:50 +0200

    remove unused functions

--- MELD/utils/__init__.py
+++ MELD/utils/__init__.py
@@ -1,5 +1,2 @@
-from .utils import construct_image_ref
-from .utils import download_file
-from .utils import load_yaml
-from .utils import safe_filename_from_url
+from .utils import construct_image_ref, load_yaml
 from .validation import validate_required_features, validate_feature_datatypes, get_unexpected_features
